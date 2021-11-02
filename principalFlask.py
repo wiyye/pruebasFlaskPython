06be3d0ebe3c5479c209c7ef974520c7b212a5dd
wiyye <guillermogarciadiaz7@gmail.com>
2021-11-02 16:39:35 +0000

    agregado de métodos martes, python y concepto

--- principalFlask.py
+++ principalFlask.py
@@ -6,5 +6,29 @@ aplicacion = Flask(__name__)
 def bienvenida():
     return "bienvenido a la página"
 
+@aplicacion.route("/martes")
+def metodoMartes():
+    return "es el segundo día de la semana"
+
+@aplicacion.route("/python")
+def metodoPython():
+    return "es un lenguaje de programación"
+
+@aplicacion.route("/concepto")
+def conceptoS():
+    elconcepto=request.args.get("laVariable1")
+    if elconcepto=="bit":
+        return "unidad minima de informacion con valores 0 o 1"
+    elif elconcepto == "byte":
+        return "equivale a 8 bits"
+    elif elconcepto == "perrito":
+        return "mejor amigo del hombre"
+    else:
+        return "concepto no encontrado"    
+
+    
+
+    
+
 if __name__ == "__main__":
     aplicacion.run(debug=True)
